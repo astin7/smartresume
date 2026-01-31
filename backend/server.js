baf49a13b4aafbc5e4b972260833e8f991e88492
andrewlisa425-ctrl <andrewlisa425@gmail.com>
2026-01-31 01:07:23 -0500

    Working Connection to mongo and hosting

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const analysisRoute = require("./routes/analysis");
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-app.use("/analysis", analysisRoute);
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5050;
-const mongo_url = process.env.MONGOURL;
-
-mongoose.connect(mongo_url, {
-  userNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-})
+const mongo_url = process.env.MONGO_URI;
+mongoose.connect(mongo_url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+   });
+  })
 .catch((error) =>{
   console.error("MongoDB connection error:", error);
 });
