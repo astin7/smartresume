f5344e3fe56bab24f065c07db35a148a56062c23
Astin Huynh <astinhuynh@gmail.com>
2026-07-01 02:35:06 -0400

    changed baseURL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://localhost:5050'
+  baseURL: 'https://smartresume-api-pbz5.onrender.com'
 });
 
 API.interceptors.request.use((req) => {
